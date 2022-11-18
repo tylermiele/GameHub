711e658f3bfde96be96025cce62f15ac8853d907
Tyler Miele <tylermiele10@gmail.com>
2022-11-18 13:44:47 -0500

    Added Stripe payment

--- GameHub/Controllers/ShopController.cs
+++ GameHub/Controllers/ShopController.cs
@@ -3,6 +3,14 @@ using GameHub.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Stripe;
+using Stripe.BillingPortal;
+using Stripe.Checkout;
+using System.Configuration;
+using Product = GameHub.Models.Product;
+using Session = Stripe.Checkout.Session;
+using SessionCreateOptions = Stripe.Checkout.SessionCreateOptions;
+using SessionService = Stripe.Checkout.SessionService;
 
 namespace GameHub.Controllers
 {
@@ -11,10 +19,15 @@ namespace GameHub.Controllers
         // add DB connection dependency object so we can do CRUD
         private readonly ApplicationDbContext _context;
 
+        //add configuration dependacy so we can read the Stripe key from appsetting or the Azure app vars section
+        //controller cannot do this otherwise
+        private readonly IConfiguration _configuration;
+
         //constructor runs automatically when calling this controller and uses our DB connection
-        public ShopController(ApplicationDbContext context)
+        public ShopController(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
         public IActionResult Index()
         {
@@ -40,7 +53,7 @@ namespace GameHub.Controllers
 
             //check if this user already has this item in their cart. SingleOrDefault selects only 1 record or nothing
             var cartItem = _context.CartItems.SingleOrDefault(c => c.ProductId == id && c.CustomerId == GetCartIdentifier());
-            if(cartItem == null)
+            if (cartItem == null)
             {
                 //create and save a new CartItm to the DB
                 cartItem = new CartItem
@@ -58,7 +71,7 @@ namespace GameHub.Controllers
                 cartItem.Quantity += 1;
                 _context.Update(cartItem);
             }
-           
+
 
             _context.SaveChanges();
 
@@ -69,7 +82,7 @@ namespace GameHub.Controllers
         {
             //is there already a session var with a cart id?
             //we need to use a session var because HTTP is stateless - variables do not perist between HTTP requests
-            if(HttpContext.Session.GetString("CartIdentifier") == null)
+            if (HttpContext.Session.GetString("CartIdentifier") == null)
             {
                 //if not, use a GUID to create a unique id and store in a new session var
                 HttpContext.Session.SetString("CartIdentifier", Guid.NewGuid().ToString());
@@ -98,7 +111,7 @@ namespace GameHub.Controllers
         public IActionResult Category(string Name)
         {
             // ensure some category name was passed in
-            if(Name == null)
+            if (Name == null)
             {
                 return RedirectToAction("Index");
             }
@@ -110,7 +123,7 @@ namespace GameHub.Controllers
             var products = new List<Product>();
 
             //use loop to make some products
-            for(var i = 1; i < 6; i++)
+            for (var i = 1; i < 6; i++)
             {
                 products.Add(new Product { ProductId = i, Name = "Product " + i.ToString(), Price = 10 + i });
             }
@@ -136,6 +149,7 @@ namespace GameHub.Controllers
         }
 
         //POST: /Shop/Checkout => save all order and customer data to a session var
+        [Authorize]
         [HttpPost]
         public IActionResult Checkout([Bind("FirstName,LastName,Address,City,Province,PostalCode,Phone")] Order order)
         {
@@ -155,5 +169,105 @@ namespace GameHub.Controllers
             // start Stripe payment
             return RedirectToAction("Payment");
         }
+
+        //GET: /Shop/Payment => show payment start form to launch Stripe
+        [Authorize]
+        public IActionResult Payment()
+        {
+            return View();
+        }
+
+        //POST: /Shop/ProcessPayment => invoke Stripe payment and process response
+        [HttpPost]
+        [Authorize]
+        public IActionResult ProcessPayment()
+        {
+            //get order from session var
+            var order = HttpContext.Session.GetObject<Order>("Order");
+            var orderTotal = order.OrderTotal;
+
+            //read Stripe key from settings
+            StripeConfiguration.ApiKey = _configuration.GetValue<string>("Stripe:SecretKey");
+
+            // create payment object for Stripe
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<String>
+                {
+                    "card"
+                },
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long?)(orderTotal * 100),
+                            Currency = "cad",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = "GameHub Purchase"
+                            }
+                        },
+                        Quantity = 1
+                    }
+                },
+                Mode = "payment",
+                SuccessUrl = "https://" + Request.Host + "/Shop/SaveOrder",
+                CancelUrl = "https://" + Request.Host + "/Shop/Cart"
+            };
+
+            // execute payment
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            // process response
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+
+        //GET: /Shop/SaveOrder => save order and details to DB, clear cart after successful payment
+        [Authorize]
+        public IActionResult SaveOrder()
+        {
+            //get order from sesion var
+            var order = HttpContext.Session.GetObject<Order>("Order");
+            order.PaymentCode = HttpContext.Session.GetString("CartIdentifier");
+
+            //save order to DB and get the new OrderId
+            _context.Add(order);
+            _context.SaveChanges();
+
+            //copy each CartItem in the session to the DB as a new OrderDetail for this new Order
+            var cartItems = _context.CartItems.Where(c => c.CustomerId == HttpContext.Session.GetString("CartIdentifier"));
+            foreach(var item in cartItems)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity,
+                    Price = item.Price,
+                    OrderId = order.OrderId
+                };
+                _context.Add(orderDetail);
+            }
+            _context.SaveChanges();
+
+            //remove all CartItems
+            foreach (var item in cartItems)
+            {
+                _context.Remove(item);
+            }
+            _context.SaveChanges();
+
+            // clear session vars
+            HttpContext.Session.SetObject("Order", "");
+            HttpContext.Session.SetInt32("ItemCount", 0);
+
+            //redirect to Order Confirmation page
+            return RedirectToAction("Details", "Orders", new {@id = order.OrderId});
+
+        }
     }
 }
+
